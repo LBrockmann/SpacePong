395f92d378306b30ebf8e71bbf6b8ef6e59e244a
Luke Brockmann <lb3297@nyu.edu>
2019-09-05 16:39:54 -0400

    Bullets move and respawn now.

--- Assets/Scripts/bulletScript.cs
+++ Assets/Scripts/bulletScript.cs
@@ -1,10 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Xml.Linq;
 using UnityEngine;
 
 public class bulletScript : MonoBehaviour
 {
     public float bulletSpeed;
+    public float resetPoint;
+    public bool moveLeft;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +18,35 @@ public class bulletScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate((Vector3.right * bulletSpeed * Time.deltaTime));
+        if (moveLeft == false)
+        {
+            transform.Translate((Vector3.right * bulletSpeed * Time.deltaTime));
+
+            if (transform.position.x > 6.8f)
+            {
+                transform.position = new Vector3(-7.15f, transform.position.y, transform.position.z);
+            }
+        }
+
+        if (moveLeft)
+        {
+            transform.Translate((Vector3.left * bulletSpeed * Time.deltaTime)); 
+            if (transform.position.x < -7.15f)
+            {
+                transform.position = new Vector3(6.8f, transform.position.y, transform.position.z);
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player1"))
+        {
+            
+        }
+        if (other.gameObject.CompareTag("Player2"))
+        {
+            
+        }
     }
 }
